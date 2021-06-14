27ddf5712aa2f8b044362e9a95fee215bc8c74bb
Recep GUDEK <rcpgdk590@gmail.com>
2021-06-14 13:00:33 +0200

    eu5 review work 1

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -3,6 +3,8 @@ public class Practice1 {
     public static void main(String[] args) {
         System.out.println("EU5 is liking git on Intellij");
 
+        System.out.println("work 1");
+
     }
 
 }
